97002387257df67c17d60a53c40b7780be7e3d27
ureshinoSayumi <68464378+ureshinoSayumi@users.noreply.github.com>
2020-09-12 02:47:15 +0800

    first commit

---
+++ config/dev.env.js
@@ -0,0 +1,10 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  APIPATH: '"https://vue-course-api.hexschool.io"',
+  CUSTOMPATH: '"tup231"',
+
+})
